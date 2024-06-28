fa42f56cab9e018125787386a95040f9cd187096
Faisal Fawad <faisalfawad2004@gmail.com>
2024-06-27 21:59:20 -0400

    Fix build for GitHub

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), svgr()]
+  plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      external: ['three']
+    }
+  }
 })
