9ecd98ce737ae1f20b95ac9ad66de0ab4dbee9d5
Faisal Fawad <faisalfawad2004@gmail.com>
2024-09-23 19:18:10 -0400

    Add Posthog

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import posthog from 'posthog-js';
+import { PostHogProvider} from 'posthog-js/react'
+
+posthog.init(
+  'phc_orvs411FhN5DR4VqjBZ0P0hqYrbaYjlcNWyFuMCYoE3',
+  {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only',
+  }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
