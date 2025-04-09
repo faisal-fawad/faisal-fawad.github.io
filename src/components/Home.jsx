e37ddd7dfcc3eb55e4bb3c3f8598cc794023ac69
Faisal Fawad <faisalfawad2004@gmail.com>
2025-04-08 23:56:53 -0400

    Simplification ✨

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,9 @@ import Socials from './Socials.jsx'
 
 export default function Home() {
   return (
-    <section id="home" className="max-w-[600px] mx-auto py-12 my-10 text-slate-900 dark:text-slate-200 px-[10px] box-content">
-      <h1 className="text-7xl max-[625px]:text-6xl font-bold">
-        {[[..."Hi,"], [..."I'm"], [..."Faisal"]].map((word, i) => {
+    <section id="home" className="max-w-[600px] mx-auto pt-12 mt-10 text-slate-900 dark:text-slate-200 px-[10px] box-content">
+      <h1 className="text-5xl max-[625px]:text-4xl font-bold">
+        {[[..."Faisal"], [..."Fawad"]].map((word, i) => {
           return (
             <div key={i} className='inline-block whitespace-nowrap'>
               {word.map((letter, j) => (
@@ -18,8 +18,13 @@ export default function Home() {
           )
         })}
       </h1>
-      <h2 className="my-4 max-[625px]:text-sm">Software Developer | Problem Solver | Innovator</h2>
+      <h2 className="mt-5 text-lg max-[625px]:text-base">
+        prev @ <a className='underline' href='https://www.ericsson.com/' target='_blank'>Ericsson</a>,
+        {' '}<a className='underline' href='https://www.canadalife.com/' target='_blank'>Canada Life</a>, 
+        {' '}<a className='underline' href='https://ovc.uoguelph.ca/' target='_blank'>Ontario Veterinary College</a>
+      </h2>
       <Socials/>
+      <img src="images/back.png" alt="Backdrop" className="aspect-square object-cover rounded z-10" />
     </section>
   )
 }
