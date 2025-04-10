f834982ec66ebe74b4f7c078efe34bf80cb62c73
Faisal Fawad <faisalfawad2004@gmail.com>
2025-04-09 21:35:59 -0400

    styling...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,31 @@
 import Socials from './Socials.jsx'
 
 export default function Home() {
+  const experiences = [
+    {
+      name: "Tesla",
+      link: "https://www.tesla.com/",
+      src: "logos/tesla.jpg",
+    },
+    {
+      name: "Ericsson",
+      link: "https://www.ericsson.com/",
+      src: "logos/ericsson.jpg",
+    },
+    {
+      name: "Canada Life",
+      link: "https://www.canadalife.com/",
+      src: "logos/canada_life.jpg",
+    },
+    {
+      name: "Ontario Veterinary College",
+      link: "https://ovc.uoguelph.ca/",
+      src: "logos/guelph.jpg",
+    },
+  ];
+  
   return (
-    <section id="home" className="max-w-[600px] mx-auto pt-12 mt-10 text-slate-900 dark:text-slate-200 px-[10px] box-content">
+    <section id="home" className="max-w-[500px] mx-auto py-12 my-10 text-slate-900 dark:text-slate-200 px-[10px] box-content">
       <h1 className="text-5xl max-[625px]:text-4xl font-bold">
         {[[..."Faisal"], [..."Fawad"]].map((word, i) => {
           return (
@@ -18,10 +41,27 @@ export default function Home() {
           )
         })}
       </h1>
-      <h2 className="mt-5 text-lg max-[625px]:text-base">
-        prev @ <a className='underline' href='https://www.ericsson.com/' target='_blank'>Ericsson</a>,
-        {' '}<a className='underline' href='https://www.canadalife.com/' target='_blank'>Canada Life</a>, 
-        {' '}<a className='underline' href='https://ovc.uoguelph.ca/' target='_blank'>Ontario Veterinary College</a>
+      <ul className="flex gap-5 my-5 text-3xl max-[625px]:text-2xl">
+        <li>🖥️</li>
+        <li>🎮</li>
+        <li>🎥</li>
+        <li>🎨</li>
+        <li>✈️</li>
+      </ul>
+      <h2 className="text-lg max-[625px]:text-base">
+        I've worked @ {experiences.map((exp, index) => {
+          let delim = ""
+          if (index == experiences.length - 2) delim = ", and "
+          else if (index < experiences.length - 1) delim = ", "
+          return (
+            <span key={index}>
+              <a key={index} href={exp.link} target='_blank'>
+                {exp.name}
+              </a>
+              {delim}
+            </span>
+          )
+        })}
       </h2>
       <Socials/>
       <img src="images/back.png" alt="Backdrop" className="aspect-square object-cover rounded z-10" />
